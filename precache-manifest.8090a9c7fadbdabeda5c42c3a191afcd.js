4fab8922dd1f63750e7ce6b8f235a218b116e1b5
yousecase <yousecase@users.noreply.github.com>
2019-12-26 23:27:02 +0900

    deploy version 0.1.0

---
+++ precache-manifest.8090a9c7fadbdabeda5c42c3a191afcd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f21d5ef1ec094dc7b1ef9298ec67c5f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f87cd6571a1350e4f51c",
+    "url": "./static/js/2.310c9dc9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.310c9dc9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c0ab4497194fea0edd5e",
+    "url": "./static/js/main.4035e423.chunk.js"
+  },
+  {
+    "revision": "e52a9156c987807b782a",
+    "url": "./static/js/runtime-main.b16049ef.js"
+  }
+]);
